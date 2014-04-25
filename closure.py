7fcb6b7f832a5df42ff885afa5242c9ef28cdf15
François Vincent <francois.vincent01@gmail.fr>
2014-04-25 15:25:01 +0200

    added closure.py

---
+++ closure.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+# The simplest and most elegant way IMHO to make a closure that
+# can modify its context and that is working in py2 and py3 as well.
+
+import sys
+if sys.version_info[0] == 3:
+    raw_input = input
+
+
+def make_authorize(message='proceed ? ', grant='yes', granted=False):
+    outer = locals()
+    def process():
+        if outer['done']:
+            return True
+        if raw_input(message) == grant:
+            outer['done'] = True
+            return True
+    return process
+
+authorize = make_authorize()
+
+if __name__ == '__main__':
+    if authorize():
+        print('authorize !')
+    if authorize():
+        print('authorize !')
