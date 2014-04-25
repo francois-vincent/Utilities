5b0de853a091863c1385ba5ade78d5bd30b2588c
François Vincent <francois.vincent01@gmail.fr>
2014-04-25 19:10:03 +0200

    refactored singleton

--- singleton.py
+++ singleton.py
@@ -1,23 +1,19 @@
 # -*- coding: utf-8 -*-
 
 """
-2 nice ways to handle singleton
+2 concurrent ways to handle singleton.
+Decorator is my favorite
 """
 
-__version__ = '0.1.0'
-__date__    = 'jan 03rd, 2013'
-__author__ = 'François Vincent'
-__mail__ = 'fvincent@groupeseb.com'
-__github__ = 'https://github.com/francois-vincent'
 
 def singleton(cls):
-    instance = None
     outer = locals()
     def getinstance(*args, **kwargs):
-        if not outer['instance']:
+        if 'instance' not in outer:
             outer['instance'] = cls(*args, **kwargs)
         return outer['instance']
     return getinstance
+
 
 class Singleton(type):
     __instance = None
@@ -26,23 +22,25 @@ class Singleton(type):
             Singleton.__instance = super(Singleton, cls).__call__(*args, **kwargs)
         return Singleton.__instance
 
-class MyClass(object):
-    def __init__(self):
-        print '__init__', self.__class__.__name__
-a = MyClass()
-b = MyClass()
-print a is b
-
-@singleton
-class MyClass2(MyClass):
-    pass
-a = MyClass2()
-b = MyClass2()
-print a is b
+if __name__ == '__main__':
+    class MyClass(object):
+        def __init__(self):
+            print '__init__', self.__class__.__name__
+    a = MyClass()
+    b = MyClass()
+    print a is b
 
 
-class MyClass3(MyClass):
-    __metaclass__ = Singleton
-a = MyClass3()
-b = MyClass3()
-print a is b
+    @singleton
+    class MyClass2(MyClass):
+        pass
+    a = MyClass2()
+    b = MyClass2()
+    print a is b
+
+
+    class MyClass3(MyClass):
+        __metaclass__ = Singleton
+    a = MyClass3()
+    b = MyClass3()
+    print a is b
