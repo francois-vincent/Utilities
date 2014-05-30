5e3c16f7d4dc6e919fbb554e13e1e25f1ab5fc5d
François Vincent <francois.vincent@polyconseil.fr>
2014-05-30 19:02:43 +0200

    fix for py3, tested of for pypy

--- closure.py
+++ closure.py
@@ -2,13 +2,16 @@
 
 # The simplest and most elegant way IMHO to make a closure that
 # can modify its context and that is working in py2 and py3 as well.
+# tested with pypy too.
 
 import sys
 if sys.version_info[0] == 3:
     raw_input = input
+    xrange = range
 
 
 def make_authorize(message='proceed ? ', grant='yes', granted=False):
+    # create dictionary of local variables
     outer = locals()
     def process():
         if outer['granted']:
