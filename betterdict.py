b6a7ecf8e9f6f2b88a637d9fb018ce7edb76e3e6
François Vincent <francois.vincent@polyconseil.fr>
2014-06-15 19:27:59 +0200

    new singleton factory, betterdict first version

---
+++ betterdict.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+
+class bdict(dict):
+    """
+    Better dict redifines update to return self.
+    In a general way, each method that do not return a datum should return self, so as to allow method chaining.
+    It also defines new methods:
+    add() adds only new keys.
+    remove() removes keys from parameter tuple/list.
+    __add__() updates from lhs dict.
+    __sub__() removes keys from lhs dict.
+    """
+    def update(self, E=None, **F):
+        dict.update(self, E, **F) if E else dict.update(self, **F)
+        return self
+
+    def add(self, E=None, **F):
+        return self.update(bdict().update(E, **F).__sub__(self))
+
+    def remove(self, *keys):
+        if len(keys) == 1 and isinstance(keys, (tuple, list)):
+            keys = keys[0]
+        for k in keys:
+            try:
+                del self[k]
+            except KeyError:
+                pass
+        return self
+
+    def __add__(self, other):
+        dict.update(self, other)
+        return self
+
+    def __sub__(self, other):
+        for k in other.iterkeys():
+            try:
+                del self[k]
+            except KeyError:
+                pass
+        return self
+
+
+if __name__ == '__main__':
+    d = bdict(a=1, b=2)
+    d2 = dict(c=3)
+    d3 = dict(d=4)
+    assert 'c' in d.update(d2)
+    d += d3
+    assert d == {'a': 1, 'c': 3, 'b': 2, 'd': 4}
+    d -= d2
+    assert d == {'a': 1, 'b': 2, 'd': 4}
+    d = bdict(a=1, b=2, c=3)
+    d.remove('a')
+    assert d == {'c': 3, 'b': 2}
+    d = bdict(a=1, b=2, c=3)
+    d.remove('a', 'b')
+    assert d == {'c': 3}
+    d = bdict(a=1, b=2, c=3)
+    d.remove(('a',))
+    assert d == {'c': 3, 'b': 2}
+    d = bdict(a=1, b=2, c=3)
+    d.remove(('a', 'b'))
+    assert d == {'c': 3}
+    assert bdict(a=1, b=2).update(b=3, c=4) == {'a': 1, 'b': 3, 'c': 4}
+    assert bdict(a=1, b=2).add(b=3, c=4) == {'a': 1, 'b': 2, 'c': 4}
--- singleton.py
+++ singleton.py
@@ -1,12 +1,16 @@
 # -*- coding: utf-8 -*-
 
 """
-2 concurrent ways to handle singleton.
-Decorator is my favorite
+3 different ways to handle singleton.
+A decorator, a metaclass and a mixin/factory.
 """
 
 
 def singleton(cls):
+    """
+    Class decorator
+    TODO: use args and kwargs as key to instance dict
+    """
     outer = locals()
     def getinstance(*args, **kwargs):
         if 'instance' not in outer:
@@ -15,12 +19,28 @@ def singleton(cls):
     return getinstance
 
 
-class Singleton(type):
+class _singleton(type):
+    """
+    Metaclass
+    """
     __instance = None
     def __call__(cls, *args, **kwargs):
-        if Singleton.__instance is None:
-            Singleton.__instance = super(Singleton, cls).__call__(*args, **kwargs)
-        return Singleton.__instance
+        if _singleton.__instance is None:
+            _singleton.__instance = super(_singleton, cls).__call__(*args, **kwargs)
+        return _singleton.__instance
+
+
+class Singleton(object):
+    """
+    Mixin with factory
+    """
+    _instance = None
+    @classmethod
+    def get_instance(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = cls(*args, **kwargs)
+        return cls._instance
+
 
 if __name__ == '__main__':
     class MyClass(object):
@@ -28,19 +48,23 @@ if __name__ == '__main__':
             print '__init__', self.__class__.__name__
     a = MyClass()
     b = MyClass()
-    print a is b
-
+    print 'Singleton:', a is b
 
     @singleton
     class MyClass2(MyClass):
         pass
     a = MyClass2()
     b = MyClass2()
-    print a is b
-
+    print 'Singleton:', a is b
 
     class MyClass3(MyClass):
-        __metaclass__ = Singleton
+        __metaclass__ = _singleton
     a = MyClass3()
     b = MyClass3()
-    print a is b
+    print 'Singleton:', a is b
+
+    class MyClass4(Singleton, MyClass):
+        pass
+    a = MyClass4.get_instance()
+    b = MyClass4.get_instance()
+    print 'Singleton:', a is b
