afd45350b7128ecd962a4d58e03986c2654081a6
francois.vincent <francois.vincent@kosc-telecom.fr>
2017-07-27 17:01:38 +0200

    added decorators.py

---
+++ decorators.py
@@ -0,0 +1,12 @@
+# Copyright 2016 Kosc Telecom
+
+def memoize(f):
+    """ Memoization decorator for a function with positional args only
+    """
+    class memodict(dict):
+        def __getitem__(self, *key):
+            return dict.__getitem__(self, key)
+        def __missing__(self, key):
+            ret = self[key] = f(*key)
+            return ret
+    return memodict().__getitem__
