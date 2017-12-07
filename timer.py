0bd5be04524a0449d27f7cf75844a8f2fc71c0db
francois.vincent <francois.vincent@kosc-telecom.fr>
2017-12-07 17:22:41 +0100

    added a callback to Timer, added a gtk3 test

--- timer.py
+++ timer.py
@@ -4,11 +4,14 @@ import time
 
 
 class Timer(object):
-    def __init__(self):
+    def __init__(self, callback=None):
         self.start = time.time()
+        self.callback = callback
     def __enter__(self):
         return self
     def __exit__(self, *args):
         self.duration = time.time() - self.start
+        if self.callback:
+            self.callback(self.duration)
     def get(self):
         return self.duration
