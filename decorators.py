46f6800e81913dce2d469222f41d215caa46a761
francois.vincent <francois.vincent@kosc-telecom.fr>
2018-01-05 12:48:15 +0100

    add a memoize deco for methods

--- decorators.py
+++ decorators.py
@@ -1,6 +1,8 @@
 
+from functools import partial
 
-def memoize(f):
+
+def memoize_f(f):
     """ Memoization decorator for a function with positional args only
     """
     class memodict(dict):
@@ -12,13 +14,31 @@ def memoize(f):
     return memodict().__getitem__
 
 
+class memoize(object):
+    """ Memoization decorator for a method with positional args only
+    """
+    def __init__(self, func):
+        self.func = func
+        self.cache = {}
+    def __call__(self, *args):
+        if args in self.cache:
+            return self.cache[args]
+        else:
+            value = self.cache[args] = self.func(*args)
+            return value
+    def __doc__(self):
+        return self.func.__doc__
+    def __get__(self, obj, objtype):
+        return partial(self.__call__, obj)
+
+
 if __name__ == '__main__':
     class A(object):
         def __init__(self):
             self.x = 0
         @memoize
         def sum(self, *args):
-            self.x += sum(*args)
+            self.x += sum(args)
             return self.x
 
     a = A()
