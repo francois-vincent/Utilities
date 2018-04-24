d02e34429c6f76d37649fd52ff0ed4596e5b9acc
François <snoopyparis@gmail.com>
2018-04-24 19:45:22 +0200

    fix and enhance UndoIterator

--- undo_iterator.py
+++ undo_iterator.py
@@ -10,9 +10,9 @@ class UndoIterator(object):
 
     def __init__(self, iterator, depth=5):
         self.iterator = iterator
-        self.depth = depth
         self.queue = deque(maxlen=depth+1) if depth > 0 else deque()
         self.back_queue = []
+        self.index = -1
 
     def __iter__(self):
         return self
@@ -20,15 +20,20 @@ class UndoIterator(object):
     def next(self):
         elt = self.back_queue.pop() if self.back_queue else next(self.iterator)
         self.queue.append(elt)
+        self.index += 1
         return elt
 
     def prev(self):
         if not self.back_queue:
-            self.back_queue.append(self.queue.pop())
+            self.repeat()
         elt = self.queue.pop()
         self.back_queue.append(elt)
+        self.index -= 1
         return elt
-    back = undo = prev
+
+    def repeat(self):
+        self.back_queue.append(self.queue.pop())
+        self.index -= 1
 
     def cur(self):
         return self.queue[-1]
@@ -37,11 +42,14 @@ class UndoIterator(object):
 if __name__ == '__main__':
     ui = UndoIterator(iter('ABCDE'))
     for i, x in enumerate(ui):
-        print(i, x)
+        print(i, x, ui.index)
         if i == 2:
             print("current: %s" % ui.cur())
         if i == 3:
-            print("undo:    %s" % ui.undo())
+            print("undo:    %s" % ui.prev())
         if i == 6:
-            print("undo:    %s" % ui.undo())
-            print("undo:    %s" % ui.undo())
+            print("undo:    %s" % ui.prev())
+            print("undo:    %s" % ui.prev())
+        if 7 <= i <= 8:
+            print("repeat")
+            ui.repeat()
